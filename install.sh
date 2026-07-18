71a8a322f7c1187a115dfcd2a674f6be500cbdd6
Fish <153741180+Adithyan-R-14@users.noreply.github.com>
2026-07-18 18:32:14 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+chmod +x oneshot.py
+chmod +x wps.crack.py
+
+sudo cp oneshot.py /usr/local/bin/oneshot.py
+sudo cp wps.crack.py /usr/local/bin/wps.crack
+
+
+sudo chmod +x /usr/local/bin/oneshot.py
+sudo chmod +x /usr/local/bin/wps.crack
+
+echo "INSTALLD"
